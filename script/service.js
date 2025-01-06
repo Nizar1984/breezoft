8a22fcfe667a6aa4db5cba3710f74ccbbf8dd2a8
Nizar1984 <tonizaruae@gmail.com>
2025-01-06 21:53:25 +0530

    service page and login page updated with details form visible/invisible

---
+++ script/service.js
@@ -0,0 +1,18 @@
+const moreDetails = document.querySelector('.details');
+const hideForm = document.querySelector('.more');
+const close = document.querySelector('.close');
+moreDetails.addEventListener('click', function () {
+    hideForm.classList.remove('hidden');
+
+});
+
+close.addEventListener('click', function () {
+    hideForm.classList.add('hidden')
+});
+
+document.addEventListener('keydown', function (e) {
+    e.key === 'Escape' && !hideForm.classList.contains('hidden') ? hideForm.classList.add('hidden') : null;
+      
+})
+
+
